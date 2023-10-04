9d108bf572482d258158a201885c29f49c4c01b5
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-10-04 10:35:30 -0300

    feat: install Laravel Breeze

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\api\v1\CadastroEstabelecimentoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,19 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::prefix('v1')->group(function () {
-    Route::resource('/cadastro-estabelecimento', CadastroEstabelecimentoController::class)->only([
-        'index', 'show', 'store', 'update', 'destroy'
-    ]);
-});
-
-
