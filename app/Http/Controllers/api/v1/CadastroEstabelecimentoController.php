d56955a7218435067afd91d5e4b4d96a5da7c527
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-10-04 10:31:06 -0300

    feat: CRUD Estabelecimento

--- app/Http/Controllers/api/v1/CadastroEstabelecimentoController.php
+++ app/Http/Controllers/api/v1/CadastroEstabelecimentoController.php
@@ -3,12 +3,62 @@
 namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cadastro_estabelecimento;
+use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CadastroEstabelecimentoController extends Controller
 {
     public function index (): JsonResponse
     {
-        return response()->json('Teste');
+        $estabelecimento =  Cadastro_estabelecimento::all();
+        return response()->json(compact('estabelecimento'));
+    }
+
+    public function store (Request $request): JsonResponse
+    {
+        try {
+            $validate = $request->validate([
+                'name' => 'required',
+                'cnpj' => 'required',
+                'phone' => 'required',
+                'number_motorcycles_spaces' => 'required',
+                'number_car_spaces' => 'required'
+            ]);
+
+            $estabelecimento = Cadastro_estabelecimento::create($validate);
+
+            return response()->json(['Estabelecimento' => $estabelecimento], 201);
+        }catch (Exception $e){
+            return response()->json(["error" => $e->getMessage()], 500);
+        }
+    }
+
+    public function show (string $id): JsonResponse
+    {
+        $estabelecimento = Cadastro_estabelecimento::find($id);
+        return response()->json(['Estabelecimento' => $estabelecimento]);
+    }
+
+    public function update (Request $request, string $id): JsonResponse
+    {
+        try {
+            $estabelecimento = Cadastro_estabelecimento::find($id);
+            $estabelecimento->update($request->all());
+            $estabelecimento->save();
+
+            return response()->json(['Estabelecimento' => $estabelecimento], 201);
+        }catch (Exception $e){
+            return response()->json(["error" => $e->getMessage()], 500);
+        }
+    }
+
+    public function destroy (string $id): JsonResponse
+    {
+        $estabelecimento =  Cadastro_estabelecimento::find($id);
+        $estabelecimento->delete();
+
+        return response()->json(['Message' => 'Estabelecimento Removido com Sucesso!!'], 204);
     }
 }
